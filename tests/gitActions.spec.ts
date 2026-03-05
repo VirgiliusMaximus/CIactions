63c18bcf98fa9072c7bd37c95e92a5daa05b6eba
VirgiliusMaximus <virgilus@gmail.com>
2026-03-05 17:10:20 +0200

    latest

--- tests/gitActions.spec.ts
+++ tests/gitActions.spec.ts
@@ -2,57 +2,50 @@ import { test, expect } from "@playwright/test"
 
 test('Git action practice 1', async ({ page }) => {
     console.log("Start practice test 1")
-    await page.goto('https://www.saucedemo.com/');
-    console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs");
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
     console.log("End practice test 1")
+
 });
 
 test('Git action practice 2', async ({ page }) => {
     console.log("Start practice test 2")
-    await page.goto('https://www.saucedemo.com/');
-    console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs wrong");
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright2/);
     console.log("End practice test 2")
 });
 test.describe('Describe test 3,4,5', async () => {
     test('Git action practice 3', async ({ page }) => {
         console.log("Start practice test 3")
-        await page.goto('https://www.saucedemo.com/');
-        console.log(await page.title());
-        expect(page).toHaveTitle("Swag Labs");
-        console.log("End practice test 3")
+        await page.goto('https://playwright.dev/');
+        await expect(page).toHaveTitle(/Playwright/);
     });
 
     test('Git action practice 4', async ({ page }) => {
         console.log("Start practice test 4")
-        await page.goto('https://www.saucedemo.com/');
-        console.log(await page.title());
-        expect(page).toHaveTitle("Swag Labs");
+        await page.goto('https://playwright.dev/');
+        await expect(page).toHaveTitle(/Playwright/);
         console.log("End practice test 4")
     });
 
     test('Git action practice 5', async ({ page }) => {
         console.log("Start practice test 5")
-        await page.goto('https://www.saucedemo.com/');
-        console.log(await page.title());
-        expect(page).toHaveTitle("Swag Labs");
+        await page.goto('https://playwright.dev/');
+        await expect(page).toHaveTitle(/Playwright/);
         console.log("End practice test 5")
     });
 });
 
 test('Git action practice 6', async ({ page }) => {
     console.log("Start practice test 6")
-    await page.goto('https://www.saucedemo.com/');
-    console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs");
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
     console.log("End practice test 6")
 });
 
 test('Git action practice 7', async ({ page }) => {
     console.log("Start practice test 7")
-    await page.goto('https://www.saucedemo.com/');
-    console.log(await page.title());
-    expect(page).toHaveTitle("Swag Labs");
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
     console.log("End practice test 7")
 });
